bdec254cb4763cbfa0abe9501fb8bdd0e6520d08
mdijkstra-cpb <m.dijkstra.cpb@gmail.com>
2017-02-17 18:02:47 +0100

    download and create server and ui

--- server/create-server-ui.sh
+++ server/create-server-ui.sh
@@ -1,2 +1,37 @@
 # Download R files from github to create server/ui
 
+base_url=https://raw.githubusercontent.com/mdijkstracpb/tsview/master/R/
+
+R_main_files="server.R
+ui.R"
+
+R_help_files="ext-switch-button.R
+tsview-helper.R
+tsview-plot.R
+tsview-settings.R
+tsview-tsplot.R
+tsview-tsview.R"
+
+# download ui and server
+for f in ${R_main_files[@]}
+do
+	wget -O ${f}.original ${base_url}$f
+done
+
+# download help files
+for f in ${R_help_files[@]}
+do
+	wget ${base_url}$f
+done
+
+rm source-these-files.R.help
+for each in *.R
+do
+  echo "source(\"$each\")" >> source-these-files.R.help
+done
+
+# compose server and ui
+for f in ${R_main_files[@]}
+do
+  cat source-these-files.R.help ${f}.original > ${f}
+done
