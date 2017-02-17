84becb4c060610371c8c6e105324b4287c45f77a
mdijkstra-cpb <m.dijkstra.cpb@gmail.com>
2017-02-17 12:48:21 +0100

    consistent file names

--- R/tsview-helper.R
+++ R/tsview-helper.R
@@ -1,3 +1,5 @@
+#' @importFrom utils install.packages installed.packages
+#'
 getResource = function(file.name) paste0("shiny-app/www/", file.name)
 
 .tsview.lst.as.ts = function(lst, version.prefix = " (", version.postfix = ")")
@@ -12,7 +14,7 @@ getResource = function(file.name) paste0("shiny-app/www/", file.name)
 		{
 			colnames(lst[[i]]) = paste("Series", 1:ncol(lst[[i]])) # add missing colnames
 		}
-		
+
 		colnames(lst[[i]]) = paste0(colnames(lst[[i]]), version.prefix, i, version.postfix) # add version
 		x.names.version = c(x.names.version, colnames(lst[[i]])) # collect names (version)
 	}
@@ -24,7 +26,7 @@ getResource = function(file.name) paste0("shiny-app/www/", file.name)
 		x = ts.union(x, lst[[i]])
 	}
 	colnames(x) = x.names.version
-	
+
 	return(x)
 }
 
@@ -65,9 +67,40 @@ getResource = function(file.name) paste0("shiny-app/www/", file.name)
 {
 	if (is.na(time.range[1])) time.range[1] = min(time(x))
 	if (is.na(time.range[2])) time.range[2] = max(time(x))
-	
+
 	delta.t = diff(time(x))[1]
 	if (diff(time.range) < delta.t) time.range[2] = time.range[2] + delta.t
-	
+
 	time.range
+}
+
+prettyTime = function(time.label, format = F)
+{
+  time.label.floor = floor(time.label)
+
+  q1.index = which(time.label - 0    == time.label.floor)
+  q2.index = which(time.label -  .25 == time.label.floor)
+  q3.index = which(time.label -  .50 == time.label.floor)
+  q4.index = which(time.label -  .75 == time.label.floor)
+
+  addQ = function(year, q)
+  {
+    paste0(if (format) "<B>", year, if (format) '</B><font face="verdana" color="green">', q, if (format) '</font>')
+  }
+  time.label.floor[q1.index] = addQ(time.label.floor[q1.index], "Q1")
+  time.label.floor[q2.index] = addQ(time.label.floor[q2.index], "Q2")
+  time.label.floor[q3.index] = addQ(time.label.floor[q3.index], "Q3")
+  time.label.floor[q4.index] = addQ(time.label.floor[q4.index], "Q4")
+
+  time.label.floor
+}
+
+.install.if.not.installed = function(this.package)
+{
+  new.packages = this.package[!(this.package %in% utils::installed.packages()[,"Package"])]
+
+  if (length(new.packages))
+  {
+    utils::install.packages(new.packages)
+  }
 }
